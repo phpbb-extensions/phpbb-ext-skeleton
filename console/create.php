af2995b96c52fe74c78e1b050809d76e004c4492
Joas Schilling <nickvergessen@owncloud.com>
2015-05-29 15:16:28 +0200

    Add the basic questionaire

---
+++ console/create.php
@@ -0,0 +1,390 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+namespace phpbb\skeleton\console;
+
+use phpbb\config\config;
+use phpbb\path_helper;
+use phpbb\skeleton\helper\validator;
+use phpbb\user;
+use Symfony\Component\Console\Helper\DialogHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class create extends \phpbb\console\command\command
+{
+	protected $data = array();
+
+	/** @var \phpbb\skeleton\helper\validator */
+	protected $validator;
+
+	protected function get_composer_dialog_values()
+	{
+		return array(
+			'author' => array(
+				'author_name' => null,
+				'author_email' => null,
+				'author_homepage' => null,
+				'author_role' => null,
+			),
+			'extension' => array(
+				'vendor_name' => null,
+				'extension_display_name' => null,
+				'extension_name' => null,
+				'extension_description' => null,
+				'extension_version' => '1.0.0-dev',
+				'extension_homepage' => null,
+				'extension_time' => date('Y-m-d'),
+			),
+			'requirements' => array(
+				'php_version' => '5.3.3',
+				'phpbb_version_min' => '3.1.4',
+				'phpbb_version_max' => '3.2.0',
+			)
+		);
+	}
+
+	protected function get_component_dialog_values()
+	{
+		return array(
+			'phplistener' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(
+//					'config/services.yml',
+//					'event/main_listener.php',
+				),
+			),
+			'htmllistener' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(
+//					'styles/prosilver/template/event/overall_header_navigation_prepend.html',
+				),
+			),
+			'acp' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(
+//					'acp/main_info.php',
+//					'acp/main_module.php',
+//					'adm/style/demo_body.html',
+				),
+			),
+////TODO missing
+//			'language-acp' => array(
+//				'default' => true,
+//				'dependencies' => array('acp'),
+//				'files' => array(
+//					'language/en/info_acp_demo.php',
+//				),
+//			),
+			'migration' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(
+//					'migrations/release_1_0_0.php',
+//					'migrations/release_1_0_1.php',
+				),
+			),
+			'service' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(),
+			),
+			'parameter' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(),
+			),
+			'controller' => array(
+				'default' => true,
+				'dependencies' => array('service', 'route'),
+				'files' => array(),
+			),
+			'language-controller' => array(
+				'default' => true,
+				'dependencies' => array('controller'),
+				'files' => array(),
+			),
+			'tests' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(),
+			),
+			'travis' => array(
+				'default' => true,
+				'dependencies' => array('tests'),
+				'files' => array(),
+			),
+			'build' => array(
+				'default' => true,
+				'dependencies' => array(),
+				'files' => array(),
+			),
+		);
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param user $user User instance (mostly for translation)
+	 * @param path_helper $path_helper The filesystem object
+	 * @param string $root_path
+	 */
+	public function __construct(user $user, path_helper $path_helper, $root_path)
+	{
+		parent::__construct($user);
+		$this->validator = new validator($this->user);
+		$this->path_helper = $path_helper;
+		$this->root_path = $root_path;
+	}
+
+	/**
+	* {@inheritdoc}
+	*/
+	protected function configure()
+	{
+		$this
+			->setName('skeleton:create')
+			->setDescription($this->user->lang('CLI_DESCRIPTION_SKELETON_CREATE'))
+		;
+	}
+
+	/**
+	* Executes the command config:delete.
+	*
+	* Removes a configuration option
+	*
+	* @param InputInterface  $input  An InputInterface instance
+	* @param OutputInterface $output An OutputInterface instance
+	*
+	* @return null
+	* @see \phpbb\config\config::delete()
+	*/
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		/** @var DialogHelper $dialog */
+		$dialog = $this->getHelper('dialog');
+		$this->get_composer_data($dialog, $output);
+		$this->get_component_data($dialog, $output);
+
+		$this->create_extension();
+	}
+
+	/**
+	 * @param DialogHelper $dialog
+	 * @param OutputInterface $output
+	 */
+	protected function get_composer_data(DialogHelper $dialog, OutputInterface $output)
+	{
+		$dialog_questions = $this->get_composer_dialog_values();
+		foreach ($dialog_questions['extension'] as $value => $default)
+		{
+			$this->data['extension'][$value] = $this->get_user_input($dialog, $output, $value, $default);
+		}
+
+		$num_authors = $dialog->askAndValidate(
+			$output,
+			$this->user->lang('SKELETON_QUESTION_NUM_AUTHORS'),
+			array($this->validator, 'validate_num_authors'),
+			false,
+			1
+		);
+
+		$this->data['authors'] = array();
+		for ($i = 0; $i < $num_authors; $i++)
+		{
+			foreach ($dialog_questions['author'] as $value => $default)
+			{
+				$this->data['authors'][$i][$value] = $this->get_user_input($dialog, $output, $value, $default);
+			}
+		}
+
+		foreach ($dialog_questions['requirements'] as $value => $default)
+		{
+			$this->data['requirements'][$value] = $this->get_user_input($dialog, $output, $value, $default);
+		}
+	}
+
+	/**
+	 * @param DialogHelper $dialog
+	 * @param OutputInterface $output
+	 */
+	protected function get_component_data(DialogHelper $dialog, OutputInterface $output)
+	{
+		$components = $this->get_component_dialog_values();
+		foreach ($components as $component => $details)
+		{
+			foreach ($details['dependencies'] as $depends)
+			{
+				if (empty($this->data['components'][$depends]))
+				{
+					$this->data['components'][$component] = false;
+					continue 2;
+				}
+			}
+
+			$this->data['components'][$component] = $this->get_user_input($dialog, $output, 'component_' . $component, $details['default']);
+		}
+	}
+
+	protected function create_extension()
+	{
+		$ext_path = $this->root_path . 'store/tmp-ext/' . "{$this->data['extension']['vendor_name']}/{$this->data['extension']['extension_name']}/";
+		$filesystem = new \Symfony\Component\Filesystem\Filesystem();
+		$filesystem->remove($this->root_path . 'store/tmp-ext/');
+		$filesystem->mkdir($ext_path);
+
+		$template_engine = new \phpbb\template\twig\twig($this->path_helper, new config(array(
+			'load_tplcompile' => true,
+			'tpl_allow_php' => false,
+			'assets_version' => null,
+		)), $this->user, new \phpbb\template\context());
+		$template_engine->set_custom_style('skeletonextension', $this->root_path . 'ext/phpbb/skeleton/skeleton');
+
+		$template_engine->assign_vars(array(
+			'COMPONENT' => $this->data['components'],
+			'EXTENSION' => $this->data['extension'],
+			'REQUIREMENTS' => $this->data['requirements'],
+			'AUTHORS' => $this->data['authors'],
+		));
+
+		$component_data = $this->get_component_dialog_values();
+		$skeleton_files = array(
+			'README.md',
+			'license.txt',
+
+			// TODO filter by component
+			'acp/main_info.php',
+			'acp/main_module.php',
+			'adm/style/demo_body.html',
+			'config/routing.yml',
+			'config/services.yml',
+//			'config/tables.yml', // TODO missing
+			'controller/main.php',
+			'event/main_listener.php',
+			'language/en/common.php',
+			'migrations/release_1_0_0.php',
+			'migrations/release_1_0_1.php',
+			'styles/prosilver/template/event/overall_header_navigation_prepend.html',
+			'styles/prosilver/template/demo_body.html',
+
+			// TODO missing tests for now
+		);
+
+//		foreach ($this->data['components'] as $component => $selected)
+//		{
+//			if ($selected)
+//			{
+//				$skeleton_files = array_merge($skeleton_files, $component_data[$component]['files']);
+//			}
+//		}
+
+		foreach ($skeleton_files as $file)
+		{
+			$template_engine->set_filenames(array('body' => $file));
+			$filesystem->dumpFile($ext_path . $file, $template_engine->assign_display('body') . "\n");
+		}
+
+		$filesystem->dumpFile($ext_path . 'composer.json', $this->get_composer_json_from_data());
+	}
+
+	/**
+	 * @param DialogHelper $dialog
+	 * @param OutputInterface $output
+	 * @param string $value
+	 * @param mixed $default
+	 * @return mixed|string
+	 */
+	protected function get_user_input(DialogHelper $dialog, OutputInterface $output, $value, $default)
+	{
+		if (method_exists($this->validator, 'validate_' . $value))
+		{
+			$return_value = $dialog->askAndValidate(
+				$output,
+				$this->user->lang('SKELETON_QUESTION_' . strtoupper($value)),
+				array($this->validator, 'validate_' . $value),
+				false,
+				$default
+			);
+		}
+		else if (is_bool($default))
+		{
+			$return_value = $dialog->askConfirmation(
+				$output,
+				$this->user->lang('SKELETON_QUESTION_' . strtoupper($value)),
+				$default
+			);
+		}
+		else
+		{
+			$return_value = $dialog->ask(
+				$output,
+				$this->user->lang('SKELETON_QUESTION_' . strtoupper($value)),
+				$default
+			);
+		}
+
+		return $return_value;
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function get_composer_json_from_data()
+	{
+		$composer = array(
+			'name' => "{$this->data['extension']['vendor_name']}/{$this->data['extension']['extension_name']}",
+			'type' => 'phpbb-extension',
+			'description' => "{$this->data['extension']['extension_description']}",
+			'homepage' => "{$this->data['extension']['extension_homepage']}",
+			'version' => "{$this->data['extension']['extension_version']}",
+			'time' => "{$this->data['extension']['extension_time']}",
+			'license' => 'GPL-2.0',
+			'authors' => array(),
+			'require' => array(
+				'php' => ">={$this->data['requirements']['php_version']}",
+			),
+			'require-dev' => array(
+				'phpbb/epv' => 'dev-master',
+			),
+			'extra' => array(
+				'display-name' => "{$this->data['extension']['extension_display_name']}",
+				'soft-require' => array(
+					'phpbb/phpbb' => ">={$this->data['requirements']['phpbb_version_min']},<{$this->data['requirements']['phpbb_version_max']}@dev",
+				),
+			),
+		);
+
+		if ($this->data['components']['build'])
+		{
+			$composer['require-dev']['phing/phing'] = '2.4.*';
+		}
+
+		foreach ($this->data['authors'] as $i => $author_data)
+		{
+			if ($this->data['authors'][$i]['author_name'] !== '')
+			{
+				$composer['authors'][] = array(
+					'name' => "{$this->data['authors'][$i]['author_name']}",
+					'email' => "{$this->data['authors'][$i]['author_email']}",
+					'homepage' => "{$this->data['authors'][$i]['author_homepage']}",
+					'role' => "{$this->data['authors'][$i]['author_role']}",
+				);
+			}
+		}
+
+		return json_encode($composer, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES);
+	}
+}
---
+++ helper/validator.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+namespace phpbb\skeleton\helper;
+
+use phpbb\user;
+
+class validator
+{
+	/** @var user */
+	protected $user;
+
+	public function __construct(user $user)
+	{
+		$this->user = $user;
+	}
+
+	public function validate_num_authors($value)
+	{
+		if (preg_match('#^\d+$#', $value) && $value > 0 && $value <= 20)
+		{
+			return $value;
+		}
+
+		throw new \RuntimeException($this->user->lang('SKELETON_INVALID_NUM_AUTHORS'));
+	}
+
+	public function validate_extension_name($value)
+	{
+		if (preg_match('#^[a-zA-Z][a-zA-Z0-9]*$#', $value))
+		{
+			return $value;
+		}
+
+		throw new \RuntimeException($this->user->lang('SKELETON_INVALID_EXTENSION_NAME'));
+	}
+
+	public function validate_extension_time($value)
+	{
+		if (preg_match('#^\d{4}\-\d{2}\-\d{2}$#', $value))
+		{
+			return $value;
+		}
+
+		throw new \RuntimeException($this->user->lang('SKELETON_INVALID_EXTENSION_TIME'));
+	}
+
+	public function validate_extension_version($value)
+	{
+		if (preg_match('#^\d+(\.\d){1,3}(?:-(?:a|b|RC|pl)\d+)?$#', $value))
+		{
+			return $value;
+		}
+
+		throw new \RuntimeException($this->user->lang('SKELETON_INVALID_EXTENSION_VERSION'));
+	}
+
+	public function validate_vendor_name($value)
+	{
+		if (preg_match('#^[a-zA-Z][a-zA-Z0-9]*$#', $value))
+		{
+			return $value;
+		}
+
+		throw new \RuntimeException($this->user->lang('SKELETON_INVALID_VENDOR_NAME'));
+	}
+}
