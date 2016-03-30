2809eb0e858da1583bd534bd2bbef850cded6f60
Matt Friedman <maf675@gmail.com>
2016-03-30 13:22:07 -0700

    Use an ext file to prevent installing on incomptaible phpBB boards

---
+++ ext.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * @package phpBB Extension - Acme Demo
+ * @copyright (c) 2013 phpBB Group
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+namespace acme\demo;
+
+/**
+ * Class ext
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+	/**
+	 * Check whether or not the extension can be enabled.
+	 * The current phpBB version should meet or exceed
+	 * the minimum version required by this extension:
+	 *
+	 * Requires phpBB 3.1.4
+	 *
+	 * @return bool
+	 * @access public
+	 */
+	public function is_enableable()
+	{
+		return phpbb_version_compare(PHPBB_VERSION, '3.1.4', '>=');
+	}
+}
+
