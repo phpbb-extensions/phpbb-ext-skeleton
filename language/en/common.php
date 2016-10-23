e6df54ee737f69ba8f4c0235c176d9dfb282019c
Matt Friedman <maf675@gmail.com>
2016-10-23 14:39:05 -0700

    Update the documentation link

--- language/en/common.php
+++ language/en/common.php
@@ -24,7 +24,7 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'CLI_DESCRIPTION_SKELETON_CREATE'	=> 'Create a basic skeleton extension',
 	'PHPBB_SKELETON_EXT'				=> 'Create skeleton extension',
-	'PHPBB_SKELETON_EXT_DOCS'			=> '<a href="https://github.com/phpbb-extensions/phpbb-ext-skeleton/wiki">View the documentation for more information</a>.',
+	'PHPBB_SKELETON_EXT_DOCS'			=> '<a href="https://area51.phpbb.com/docs/dev/31x/extensions/phpbb_skeleton_extension.html">View the documentation for more information</a>.',
 
 	'EXTENSION_CLI_SKELETON_SUCCESS'	=> "<info>Extension created successfully.\nCopy the extension from `store/tmp-ext/` into the `ext/` folder.</info>",
 	'SKELETON_CLI_COMPOSER_QUESTIONS'	=> '<comment>Enter composer.json details (hit enter to leave an option empty)</comment>',
