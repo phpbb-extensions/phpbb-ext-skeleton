72d880c9aaeac7bca4807ac263db9e4070a53fa4
Matt Friedman <maf675@gmail.com>
2025-06-04 08:23:11 -0700

    Make skeleton compat. with phpbb3 and 4

--- helper/packager.php
+++ helper/packager.php
@@ -199,20 +199,35 @@ class packager
 			'assets_version'  => null,
 		]);
 
-		/** @var path_helper $path_helper */
-		$path_helper = $this->phpbb_container->get('path_helper');
-		/** @var filesystem $filesystem */
-		$filesystem = $this->phpbb_container->get('filesystem');
-		$environment = new environment(
-			$config,
-			$filesystem,
-			$path_helper,
-			$this->phpbb_container->getParameter('core.cache_dir'),
-			$this->phpbb_container->get('ext.manager'),
-			new loader(
-				new filesystem()
-			)
-		);
+		$container = $this->phpbb_container;
+		$path_helper = $container->get('path_helper');
+		$filesystem = $container->get('filesystem');
+		$cache_dir = $container->getParameter('core.cache_dir');
+		$ext_manager = $container->get('ext.manager');
+
+		$is_phpbb_4 = defined('PHPBB_VERSION') &&
+			phpbb_version_compare(PHPBB_VERSION, '4.0.0-dev', '>=');
+
+		$args = $is_phpbb_4
+			? [
+				$container->get('assets.bag'),
+				$config,
+				$filesystem,
+				$path_helper,
+				$cache_dir,
+				$ext_manager,
+				new loader()
+			]
+			: [
+				$config,
+				$filesystem,
+				$path_helper,
+				$cache_dir,
+				$ext_manager,
+				new loader(new filesystem())
+			];
+
+		$environment = new environment(...$args);
 
 		// Custom filter for use by packager to decode greater/less than symbols
 		$filter = new \Twig\TwigFilter('skeleton_decode', function ($string) {
@@ -225,8 +240,8 @@ class packager
 			$config,
 			new context(),
 			$environment,
-			$this->phpbb_container->getParameter('core.cache_dir'),
-			$this->phpbb_container->get('user'),
+			$cache_dir,
+			$container->get('user'),
 			[
 				new skeleton_version_compare()
 			]
